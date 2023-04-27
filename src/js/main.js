6a5e04fa9cbdb59ed71b84125f0b4cd364c44b76
Heather Jasi <u033813021@byui.edu>
2023-04-28 00:55:46 +0200

    initial commit for individual 2

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import ProductData from "./ProductData.mjs";
+import ProductListing from "./productlist.mjs";
+
+
+const productDataInstance = new ProductData(); // create a new instance of the ProductData 
+
+
+const productListingInstance = new ProductListing(); // create a new instance of the ProductData 
