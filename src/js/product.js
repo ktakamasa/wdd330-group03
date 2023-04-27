9564f4f0cbaa0495e6a66c842ba5f8879082d888
Heather Jasi <u033813021@byui.edu>
2023-04-27 22:44:17 +0200

    solved broken cart

--- src/js/product.js
+++ src/js/product.js
@@ -4,12 +4,18 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  let cart = getLocalStorage("so-cart");
-  if (cart === null) {
-    cart = [];
-  }
-  cart.push(product);
-  setLocalStorage("so-cart", cart);
+  // let cart = getLocalStorage("so-cart");
+  // if (cart === null) {
+  //   cart = [];
+  // }
+  // cart.push(product);
+  // setLocalStorage("so-cart", cart);
+
+  // let cart =[]; //make an array
+  let products = getLocalStorage("so-cart") || [];
+  products.push(product);
+  setLocalStorage("so-cart", products);
+
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
