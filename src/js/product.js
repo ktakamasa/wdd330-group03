84b9d73ede33b026e6b8147e8c73b5f9cd9c33ae
waheednazeer <waheed.nazeer@hotmail.com>
2023-04-28 08:42:15 +0500

    team activity for dynamic product

--- src/js/product.js
+++ src/js/product.js
@@ -1,23 +1,11 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  let cart = getLocalStorage("so-cart");
-  if (cart === null) {
-    cart = [];
-  }
-  cart.push(product);
-  setLocalStorage("so-cart", cart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const productId = getParam("product");
+// console.log(dataSource.findProductById(productId));
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
