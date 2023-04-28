367814bc64435e09f1e393663c71488bf6c6bac1
ktakamasa <kohtakamasa@gmail.com>
2023-04-28 23:06:44 +0100

    w2 individual

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,12 +1,13 @@
+import {renderListWithTemplate } from "./utils.mjs";
+
 function productCardTemplate(product) {
     return `<li class="product-card">
-      <a href="product_pages/index.html?product=">
-        <img src="" alt="Image of ">
-        <h3 class="card__brand"></h3>
-        <h2 class="card__name"></h2>
-        <p class="product-card__price">$</p>
-      </a>
-    </li>`
+    <a href="product_pages/index.html?product=${product.Id}">
+    <img src="${product.Image}" alt="Image of ${product.Name}"/>
+    <h3 class="card__brand">${product.Brand.Name}</h3>
+    <h2 class="card__name">${product.Name}</h2>
+    <p class="product-card__price">$${product.FinalPrice}</p></a>
+    </li>`;
   }
 
 export default class ProductListing {
@@ -22,11 +23,21 @@ export default class ProductListing {
         // our dataSource will return a Promise...so we can use await to resolve it.
         const list = await this.dataSource.getData();
         // render the list - to be completed
-        this.list;
+        
+        // filter list
+        console.log(list);
+        this.renderList(list);
     }
 
+    // render method first stretch
     renderList(list) {
-        const htmlStrings = list.map(productCardTemplate);
-        this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+        renderListWithTemplate(productCardTemplate, this.listElement, list);
     }
+    
+
+    // render method before stretch activity
+    // renderList(list) {
+    //     const htmlStrings = list.map(productCardTemplate);
+    //     this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+    // }
 }
