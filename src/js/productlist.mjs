afbba4a93fc4ea73e9dc95e8b30b8ec2ce95f7e6
Heather Jasi <u033813021@byui.edu>
2023-04-29 11:22:49 +0200

    productlist update

--- src/js/productlist.mjs
+++ src/js/productlist.mjs
@@ -11,5 +11,26 @@ export default class ProductListing {
       // our dataSource will return a Promise...so we can use await to resolve it.
       const list = await this.dataSource.getData();
       // render the list - to be completed
+      renderList() {
+        const productCards = this.productList.map(product => {
+          return productCardTemplate(product);
+        });
+    
+        this.container.innerHTML = productCards.join("");
+      }
     }
+  };
+
+
+  function productCardTemplate(product) {
+    return `
+      <li class="product-card">
+        <a href="product_pages/index.html?product=${product.id}">
+          <img src="${product.imageUrl}" alt="Image of ${product.name}">
+          <h3 class="card__brand">${product.brand}</h3>
+          <h2 class="card__name">${product.name}</h2>
+          <p class="product-card__price">$${product.price.toFixed(2)}</p>
+        </a>
+      </li>
+    `;
   }
