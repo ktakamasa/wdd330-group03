0c0a077dd576dcfe46a2e57331362e79f796fd3c
Samuele Leoni <leo22009@byui.edu>
2023-04-29 12:35:27 +0200

    Completed Discount indicator-product listing

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,12 +1,24 @@
 import { renderListWithTemplate } from './utils.mjs';
 
+function calculateDiscountPercentage(msrp, final) {
+    return Math.round((msrp - final) / msrp * 100);
+}
+
 function productCardTemplate(product) {
+    let msrp = "";
+    let msrp_val = product.SuggestedRetailPrice;
+    let final_val = product.FinalPrice;
+    if (msrp_val > final_val) {
+        msrp = `<s>$${product.SuggestedRetailPrice}</s> -${calculateDiscountPercentage(msrp_val, final_val)}%`;
+    }
+
     return `<li class="product-card">
       <a href="product_pages/index.html?product=${product.Id}">
         <img src="${product.Image}" alt="Image of ${product.NameWithoutBrand}">
         <h3 class="card__brand">${product.Brand.Name}</h3>
         <h2 class="card__name">${product.NameWithoutBrand}</h2>
-        <p class="product-card__price">$${product.FinalPrice}</p>
+        <p class="card__msrp">${msrp}</p>
+        <p class="product-card__price">$${final_val}</p>
       </a>
     </li>`
 }
