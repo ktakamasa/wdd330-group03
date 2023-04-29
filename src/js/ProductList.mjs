d27e3e09624119f273bf655e10c745fe73835eb8
Samuele Leoni <leo22009@byui.edu>
2023-04-29 11:08:29 +0200

    Finished individual activity, not sure about the filterProducts function

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,34 @@
+import { renderListWithTemplate } from './utils.mjs';
+
+function productCardTemplate(product) {
+    return `<li class="product-card">
+      <a href="product_pages/index.html?product=${product.Id}">
+        <img src="${product.Image}" alt="Image of ${product.NameWithoutBrand}">
+        <h3 class="card__brand">${product.Brand.Name}</h3>
+        <h2 class="card__name">${product.NameWithoutBrand}</h2>
+        <p class="product-card__price">$${product.FinalPrice}</p>
+      </a>
+    </li>`
+}
+
+// I'm not sure if this is exactly what we need, but it filters the list to only show 4 products, which is what the instructions say to do.
+function filterProducts(list) {
+    return list.slice(0, 4);
+}
+
+export default class ProductListing {
+    constructor(category, dataSource, listElement) {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+
+    async init() {
+        const list = await this.dataSource.getData();
+        this.renderList(list);
+    }
+
+    renderList(list) {
+        renderListWithTemplate(productCardTemplate, this.listElement, filterProducts(list));
+    }
+}
