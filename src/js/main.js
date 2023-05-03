709e80b573f390259123e114b215feb95f11d644
waheednazeer <waheed.nazeer@hotmail.com>
2023-05-03 15:37:04 +0500

    .

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,4 @@
 import ProductData from "./ProductData.mjs";
-<<<<<<< HEAD
-import ProductList from "./ProductList.mjs";
-
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const listing = new ProductList("Tents", dataSource, element);
-
-listing.init();
-=======
 import ProductListing from "./ProductList.mjs";
 import { displayCounter } from "./cartCount.mjs";
 
@@ -21,4 +12,3 @@ const productList = new ProductListing(category, dataSource, listElement);
 productList.init();
 
 displayCounter();
->>>>>>> origin/main
