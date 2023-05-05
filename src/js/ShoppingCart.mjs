c10bcdd20030047d05c9bb7a5e65243d10f599c3
waheednazeer <waheed.nazeer@hotmail.com>
2023-05-05 11:13:23 +0500

    .

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -1,61 +1,4 @@
 import { getLocalStorage } from "./utils.mjs";
-<<<<<<< HEAD
-//import { displayCounter } from "./cartCount.mjs";
-
-export default function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  if (!cartItems) {
-    return null;
-  } else {
-    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-    document.querySelector(".product-list").innerHTML = htmlItems.join("");
-  }
-}
-function renderCartTotal()
-{
-  document.querySelector(".total").innerHTML = cartTotalTemplate();
-}
-
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
-
-  return newItem;
-}
-function cartTotalTemplate() {
-  const newItem = `<div class="cart-total-grid">
-
-
-    <h3 class="cart-total">Grand Total </h3>
-    <h3 class="cart-total-price">$${cartTotal()}</h3>
-  
-</div>`;
-
-  return newItem;
-}
-
-function cartTotal() {
-  const cartItems = getLocalStorage("so-cart");
-  const price = cartItems.map((item) => (item.FinalPrice));
-  let total=price.reduce((a, b) => a + b, 0);
- 
-  return total.toFixed(2);  
-}
-
-
-=======
 
 function cartItemTemplate(item) {
     const newItem = `<li class="cart-card divider">
@@ -91,4 +34,3 @@ function cartItemTemplate(item) {
       }
     }
   }
->>>>>>> origin/main
