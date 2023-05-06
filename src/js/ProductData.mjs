176cea9173e2d86f668f3108919fe64510fc82b9
Samuele Leoni <leo22009@byui.edu>
2023-05-06 11:29:39 +0200

    put baseURL outside ProductData class

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,3 +1,4 @@
+const baseURL = import.meta.env.VITE_SERVER_URL;
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -7,16 +8,17 @@ function convertToJson(res) {
 }
 
 export default class ProductData {
-  constructor() {
-    this.baseURL = import.meta.env.VITE_SERVER_URL;
+  constructor(category) {
+    // this.category = category;
+    // this.path = `../json/${this.category}.json`;
   }
   async getData(category) {
-    const response = await fetch(`${this.baseURL}products/search/${category}`);
+    const response = await fetch(`${baseURL}products/search/${category}`);
     const data = await convertToJson(response);
     return data.Result;
   }
   async findProductById(id) {
-    const response = await fetch(`${this.baseURL}product/${id}`);
+    const response = await fetch(`${baseURL}product/${id}`);
     const data = await convertToJson(response);
     return data.Result;
   }
