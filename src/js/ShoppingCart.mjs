d068fd87cce4ae828475380e4b13f31aa50629a2
ktakamasa <kohtakamasa@gmail.com>
2023-05-06 22:22:11 +0100

    was giving image error

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -5,7 +5,7 @@ function cartItemTemplate(item) {
     const newItem = `<li class="cart-card divider">
     <a href="#" class="cart-card__image">
       <img
-        src="${item.Images["PrimaryMedium"]}"
+        src="${item.Image}"
         alt="${item.Name}"
       />
     </a>
