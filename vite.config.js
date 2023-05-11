9cbdd075b518d142ccd2c940ed076ac4a8558d73
Heather Jasi <u033813021@byui.edu>
2023-05-11 23:23:08 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,8 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
+        checkout: resolve(__dirname, "src/cart/checkout.html"),
+        checkout: resolve(__dirname, "src/checkout/index.html"),
         product: resolve(__dirname, "src/product_pages/index.html"),
         productListing: resolve(__dirname, "src/product-listing/index.html"),
       },
