bfa9ad834a70ad92ed673937383f7ef85865e71b
ktakamasa <kohtakamasa@gmail.com>
2023-05-11 20:26:13 +0100

    w04 team

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -3,3 +3,8 @@ import CheckoutProcess from "./CheckoutProcess.mjs";
 let checkout = new CheckoutProcess("so-cart");
 
 checkout.init();
+
+document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
+    e.preventDefault(); 
+    checkout.checkout();
+});
