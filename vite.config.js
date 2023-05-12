ff9f5be9c48cd2ad6cc77018cc38148de5caa12c
ktakamasa <kohtakamasa@gmail.com>
2023-05-12 16:38:51 +0100

    m

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
         product: resolve(__dirname, "src/product_pages/index.html"),
         productListing: resolve(__dirname, "src/product-listing/index.html"),
         cartCheckout: resolve(__dirname, "src/checkout/index.html"),
-        checkoutSuccess: resolve(__dirname, "src/checkout/success.html"),
+        success: resolve(__dirname, "src/checkout/success.html"),
       },
     },
   },
