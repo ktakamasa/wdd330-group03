4b9a8380b13e31c9c3db0fc7facaddd6faa2a4bf
ktakamasa <kohtakamasa@gmail.com>
2023-05-12 16:20:53 +0100

    w4 individual, need to fix alerts

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -9,5 +9,12 @@ myCheckout.init();
 
 document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
   e.preventDefault();
+
+  let myForm = document.forms[0];
+  let validity = myForm.checkValidity();
+  myForm.reportValidity();
+  if (validity) {
   myCheckout.checkout();
+  }
+
 });
