548ae7efff6fe7e1d390baa01ac910442edd0de3
Samuele Leoni <leo22009@byui.edu>
2023-05-13 20:25:43 +0200

    Finished alert task

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,8 @@
 // import { displayCounter } from "./cartCount.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
+import Alert from "./Alert.mjs";
+
+const alert = new Alert();
+alert.init();
 
 loadHeaderFooter();
